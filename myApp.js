85e1316e89fe9688536316aa8508b9cea7f32763
git_chetan <chetanpandit2011@gmail.com>
2020-04-10 23:42:45 +0530

    taking parameters from get request

--- myApp.js
+++ myApp.js
@@ -31,6 +31,18 @@ app.get('/:word/echo', function(req, res){
     const {word} = req.params;
     res.json({"echo": word})
   })
+
+
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
 app.use(express.static(__dirname + "/public"))
 
 // make app accessible to other files
