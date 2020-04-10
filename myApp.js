b7c726348fe364c5f10bcb1a153265ffda48d481
git_chetan <chetanpandit2011@gmail.com>
2020-04-10 14:39:56 +0530

    Solution of get request

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World")
+app.get('/',function(req,res){
+    res.send("Hello Express")
+})
+app.listen(PORT)
 
 // --> 7)  Mount the Logger middleware here
 
