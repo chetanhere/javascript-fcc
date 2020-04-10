9d12357b456891b8438a3731fc4a34890980765e
git_chetan <chetanpandit2011@gmail.com>
2020-04-10 19:08:52 +0530

    middleware code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Backend Challenges Boilerplate - Basic Express
+
+GLITCH_LINK=https://glitch.com/edit/#!/chetanhere-javascript-fcc
--- myApp.js
+++ myApp.js
@@ -1,68 +1,26 @@
-
 var express = require('express');
 var app = express();
-
-
-app.listen(3000)
-
-// --> 7)  Mount the Logger middleware here
-
-
-// --> 11)  Mount the body-parser middleware  here
-
-
-/** 1) Meet the node console. */
-console.log("Hello World")
-
-console.log(process.env.MESSAGE_STYLE)
-// never put space between variable name and value in the dot env file. .env file is a shell file
-
-/** 2) A first working Express Server */
-
-
-/** 3) Serve an HTML file */
+// console.log(__dirname + "/views/index.html")
 app.get('/',function(req,res){
     res.sendFile(__dirname + "/views/index.html")
-    })
-
-/** 4) Serve static assets  */
-app.use(express.static(__dirname + "/public"))
-
-/** 5) serve JSON on a specific route */
-app.get('/json',function(req,res){
-    res.json({"message":"Hello json"})
 })
 
+app.use(function(req,res,next){
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string)
+  next()
+})
 
-/** 6) Use the .env file to configure the app */
- 
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
+app.get('/json',function(req,res){
+  var response = "Hello json"
+if (process.env.MESSAGE_STYLE == "uppercase"){
+  response = response.toUpperCase()
+}
 
-
-/** 8) Chaining middleware. A Time server */
-
-
-/** 9)  Get input from client - Route parameters */
-
-
-/** 10) Get input from client - Query parameters */
-// /name?first=<firstname>&last=<lastname>
-
+    res.json({"message":response})
   
-/** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
+})
+app.use(express.static(__dirname + "/public"))
 
-
-/** 12) Get data form POST  */
-
-
-
-// This would be part of the basic setup of an Express app
-// but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
-
-//---------- DO NOT EDIT BELOW THIS LINE --------------------
-
- module.exports = app;
+// make app accessible to other files
+module.exports = app;
